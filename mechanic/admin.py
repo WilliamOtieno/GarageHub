d4a18b457e3986f12b94d3090383459abe63b26e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-13 01:59:55 +0300

    mechanic models registration

--- mechanic/admin.py
+++ mechanic/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Appointment
 
 # Register your models here.
+
+
+class MechanicAdmin(admin.ModelAdmin):
+    list_display = ["date", "time_start", "time_end", "appointment_with"]
+    list_filter = ('date', 'update_time')
+
+admin.site.register(Appointment, MechanicAdmin)
---
+++ mechanic/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Appointment
+
+
+class AppointmentForm(forms.ModelForm):
+    
+    class Meta:
+        model = Appointment
+        fields = [
+            "date",
+            "time_start",
+            "time_end",
+            "room_number",
+            "appointment_with"
+        ]
