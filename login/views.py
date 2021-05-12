dd8f89972fefc138629adf24f3544a293b92718a
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-13 01:25:26 +0300

    login views

--- login/views.py
+++ login/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.models import User, Group
+from django.shortcuts import redirect
+from django.contrib.auth.models import Group
 from django.views.generic import TemplateView
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.urls import path
-from django.conf.urls import url
 from django.contrib.auth import logout
 
 # Create your views here.
+
+def group_check(request):
+    group_name = Group.objects.all().filter(user = request.user) 
+    group_name = str(group_name[0])
+
+    if "Client" == group_name:
+        return redirect("http://localhost:8000/client/")
+    elif "Mechanic" == group_name:
+        return redirect("http://localhost:8000/mechanic/")
+        
+
+def logout_view(request):
+    logout(request)
+    return redirect("http://localhost:8000/")
+    
+
+class register_mechanic(TemplateView):
+    template_name = "register_mechanic.html"
+
+
+class register_client(TemplateView):
+    template_name = "register_client.html"
